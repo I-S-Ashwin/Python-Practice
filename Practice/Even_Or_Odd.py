2cd02ffba2341ca4daea8dd3be91810279e40e3a
I-S-Ashwin <i.s.ashwin2006@gmail.com>
2024-11-10 22:12:08 +0530

    Update Even_Or_Odd.py

--- Practice/Even_Or_Odd.py
+++ Practice/Even_Or_Odd.py
@@ -1,6 +1,9 @@
+# Check whether the given number is even or not
 def even(n):
     if(a%2==0):
+        # Returns 'Even' when conditions are met
         return "Even"
 
+# Getting the value from user
 a=int(input("Enter A Number: "))
 print("Even" if even(a) else "Odd")
